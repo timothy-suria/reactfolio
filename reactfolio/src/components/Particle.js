7ab987afaf0bbfeb6ecbdc2a08b61959765aec97
Timothy Suria <87575216+timothy-suria@users.noreply.github.com>
2024-02-27 11:23:50 +0700

    reactfolio

---
+++ reactfolio/src/components/Particle.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Particles from "tsparticles";
+
+function Particle() {
+  return 1;
+}
+
+export default Particle;
---
+++ reactfolio/src/data/experienceData.js
@@ -0,0 +1,28 @@
+const experiences = [
+  {
+    id: 1,
+    date: "2022 - 2023",
+    position: "backend engineer intern",
+    place: "V2 Indonesia",
+  },
+  {
+    id: 2,
+    date: "2023 - 2024",
+    position: "logistics department",
+    place: "PPI Monash",
+  },
+  {
+    id: 3,
+    date: "2023 - 2024",
+    position: "publicity department",
+    place: "Monash muaithai club",
+  },
+  {
+    id: 4,
+    date: "2023 - 2024",
+    position: "application manager",
+    place: "BCA Syariah",
+  },
+];
+
+export default experiences;
